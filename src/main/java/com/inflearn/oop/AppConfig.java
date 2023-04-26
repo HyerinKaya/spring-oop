55fe00e0c2bbef14f58b55735d8da811b3cb9bb1
kksks3192@gmail.com <kksks3192@gmail.com>
2023-04-26 16:29:19 +0900

    make spring container

--- src/main/java/com/inflearn/oop/AppConfig.java
+++ src/main/java/com/inflearn/oop/AppConfig.java
@@ -7,23 +7,30 @@ import com.inflearn.oop.member.MemberServiceImpl;
 import com.inflearn.oop.member.MemoryMemberRepository;
 import com.inflearn.oop.order.OrderService;
 import com.inflearn.oop.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //생성자 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
 }
--- src/main/java/com/inflearn/oop/member/MemberApp.java
+++ src/main/java/com/inflearn/oop/member/MemberApp.java
@@ -1,16 +1,20 @@
 package com.inflearn.oop.member;
 
 import com.inflearn.oop.AppConfig;
-import com.inflearn.oop.member.Grade;
-import com.inflearn.oop.member.Member;
-import com.inflearn.oop.member.MemberService;
-import com.inflearn.oop.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //Spring 컨테이너 역할
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
